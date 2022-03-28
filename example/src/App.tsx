138ca64c71271db65d6824fa09a080f466ced54b
Daniel Jones <daniel.jones@scanifly.com>
2022-03-28 13:48:51 -0600

    chore:add react native image picker to example to provide a way to get a file from the device to use for uploading

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,47 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-tus-native';
+import { StyleSheet, View, Text, Button, Image } from 'react-native';
+import { Upload } from 'react-native-tus-native';
+
+import * as ImagePicker from 'react-native-image-picker';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [uploadResult, setUploadResult] = React.useState<number | undefined>();
+  const [imageResponse, setImageResponse] = React.useState<any>();
 
+  // const exampleUpload = new Upload(  );
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+    console.log(JSON.stringify(imageResponse, null, 2));
+  }, [imageResponse]);
+
+  const pickerOptions: ImagePicker.ImageLibraryOptions = {
+    maxHeight: 200,
+    maxWidth: 200,
+    selectionLimit: 1,
+    mediaType: 'photo',
+    includeBase64: false,
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Button
+        title="open picker for single file selection"
+        onPress={async () => {
+          ImagePicker.launchImageLibrary(pickerOptions, setImageResponse);
+        }}
+      />
+      {imageResponse?.assets &&
+        imageResponse?.assets.map(({ uri }) => (
+          <View key={uri} style={styles.image}>
+            <Image
+              resizeMode="cover"
+              resizeMethod="scale"
+              style={{ width: 200, height: 200 }}
+              source={{ uri: uri }}
+            />
+          </View>
+        ))}
+      <Text>Upload Result: {uploadResult}</Text>
     </View>
   );
 }
@@ -27,5 +56,9 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  image: {
+    marginVertical: 24,
+    alignItems: 'center',
   },
 });
