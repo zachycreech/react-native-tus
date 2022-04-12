9f5d180ac239f748a1949195866ae718888df537
Daniel Jones <daniel.jones@scanifly.com>
2022-04-12 09:31:33 -0600

    feat:add an instance holder so a TusClient can be created before the RN bridge is running

---
+++ ios/RNTusClientInstanceHolder.swift
@@ -0,0 +1,33 @@
+//
+//  RNTusClientInstanceHolder.swift
+//
+//  Created by Daniel Jones on 4/10/22.
+//
+
+import Foundation
+import TUSKit
+
+@objcMembers
+public final class RNTusClientInstanceHolder : NSObject {
+
+  public static let sharedInstance = RNTusClientInstanceHolder()
+
+  public var tusClient: TUSClient?
+  
+  public func initializeBackgroundClient() {
+    let tusClient = try! TUSClient(
+      server: URL(string: "http://localhost/files")!,
+      sessionIdentifier: "TUS BG Session",
+      storageDirectory: URL(string: "TUS/background")!
+    )
+#if DEBUG
+    try! tusClient.reset()
+#endif
+    if #available(iOS 13, *) {
+      print("TUSClient attempting to schedule background tasks")
+      tusClient.scheduleBackgroundTasks()
+    }
+
+    RNTusClientInstanceHolder.sharedInstance.tusClient = tusClient
+  }
+}
