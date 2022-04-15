bf34f2def0c2ff9f73c345d0f2cbbe38e120ad08
Daniel Jones <daniel.jones@scanifly.com>
2022-04-15 11:57:32 -0600

    remove lint rule for example app

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     {
       files: ['*.ts', '*.tsx'],
       rules: {
-        '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
       },
